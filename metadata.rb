3016c8c8d1ec59f69430ea25f6b68e326ac21dca
Eric Herot <eric.github@herot.com>
2014-08-28 15:51:23 -0400

    Bump shinken v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Monitor server wrapper cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
 
-depends 'shinken', '= 1.0.1'
+depends 'shinken', '= 1.0.2'
