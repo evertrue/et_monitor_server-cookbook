ef62704a24aa1925a1f199cc4c5e6b5b4804c7cd
Eric Herot <eric.github@herot.com>
2014-08-28 13:49:02 -0400

    Version bump v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2:
+
+* Add includes for webui and broker
+
 ## 1.0.1:
 
 * First release!
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Monitor server wrapper cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'shinken', '= 1.0.1'
