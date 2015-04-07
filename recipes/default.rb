8516a40c3fe7bd911c856263457cb5a4511924d8
Eric Herot <eric.github@herot.com>
2015-04-07 17:55:41 -0400

    Auto-populate /etc/hosts with non-VPC hosts

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,14 @@
+r = search(:node,
+           "chef_environment:#{node.chef_environment} AND " \
+           'ec2_public_hostname:*')
+
+non_vpc_hosts = r.select do |n|
+  n['ec2']['network_interfaces_macs'].find { |_k, v| v['vpc_id'] }.nil?
+end
+
+node.set['et_hostname']['addl_hosts_entries'] =
+  non_vpc_hosts.map { |n| "#{n['ec2']['public_ipv4']} #{n['fqdn']}" }
+
 include_recipe 'shinken'
 include_recipe 'shinken::webui'
 include_recipe 'shinken::broker'
