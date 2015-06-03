711ebc1ef2d1d26f9391c73781ee338be4d8d2e4
Eric Herot <eric.github@herot.com>
2015-06-03 15:27:43 -0400

    Add check_remote_process command definition

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,11 @@ set['shinken']['commands']['check_http_nostatus'] = {
   'command_line' => '$NAGIOSPLUGINSDIR$/check_http -I $HOSTADDRESS$ ' \
                     '--onredirect=follow --port=$ARG1$ --expect=HTTP'
 }
+set['shinken']['commands']['check_remote_process'] = {
+  'command_name' => 'check_remote_process',
+  'command_line' => '$NAGIOSPLUGINSDIR$/check_ssh ' \
+                    '--hostname $HOSTADDRESS$ ' \
+                    "--logname #{node['shinken']['agent_user']} " \
+                    "--command='pgrep -f $ARG1$' " \
+                    "--identity=#{node['shinken']['home']}/.ssh/id_rsa"
+}
