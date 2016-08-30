880044a4342abbc13cf2bc9f51c587a27896bc88
Eric Herot <eric.github@herot.com>
2016-08-30 17:22:42 -0400

    New shinken cookbook no longer requires command_name to be defined

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 set['shinken']['install_type'] = 'source'
 set['shinken']['commands']['check_http_nostatus'] = {
-  'command_name' => 'check_http_nostatus',
   'command_line' => '$NAGIOSPLUGINSDIR$/check_http -I $HOSTADDRESS$ ' \
                     '--onredirect=follow --port=$ARG1$ --expect=HTTP'
 }
