d74102cf8cc9a494677b13fc7b6ace0c801de5b9
Eric Herot <eric.github@herot.com>
2016-08-30 17:21:12 -0400

    Move check_remote_process to the core Shinken cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,15 +4,6 @@ set['shinken']['commands']['check_http_nostatus'] = {
   'command_line' => '$NAGIOSPLUGINSDIR$/check_http -I $HOSTADDRESS$ ' \
                     '--onredirect=follow --port=$ARG1$ --expect=HTTP'
 }
-set['shinken']['commands']['check_remote_process'] = {
-  'command_name' => 'check_remote_process',
-  'command_line' => '$NAGIOSPLUGINSDIR$/check_by_ssh ' \
-                    '-H $HOSTADDRESS$ ' \
-                    "--logname #{node['shinken']['agent_user']} " \
-                    "--command='pgrep -f $ARG1$' " \
-                    '-o StrictHostKeyChecking=no ' \
-                    "--identity=#{node['shinken']['home']}/.ssh/id_rsa"
-}
 
 set['shinken']['services']['storage_inodes'] = {
   'hostgroup_name' => 'everything',
